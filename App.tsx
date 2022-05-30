38e1a0187d13325e6a8d52eb40a18109b3e7b213
shahnewaz171 <shahnewaz601@gmail.com>
2022-05-30 16:32:19 +0600

    added expo font with google font and customized colors

--- App.tsx
+++ App.tsx
@@ -1,16 +1,32 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import customStyles from './styles/customStyles';
+import { color } from './theme/color';
+import { Props } from './theme/model/types';
 
-export default function App() {
+const App: React.FC<Props> = () => {
+  const [loaded] = useFonts({
+    'Antonio-Medium' : require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold' : require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular' : require('./assets/fonts/Spartan-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return <Text>Font is loading...</Text>
+  }
+
   return (
     <SafeAreaView style={customStyles.AndroidSafeArea}>
-        <View>
-          <Text>Welcome</Text>
+        <View style={{ backgroundColor: color.lightGreen, paddingVertical: 10 }}>
+          <Text style={{ fontWeight: '600', textAlign: 'center', fontSize: 22, color: color.white }}>Welcome</Text>
+          <StatusBar style='auto' />
         </View>
     </SafeAreaView>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
---
+++ theme/model/types.ts
@@ -0,0 +1,14 @@
+export interface Props{
+    black: string,
+    white: string,
+    darkGray: string,
+    gray: string,
+    lightGreen: string,
+    orange: string,
+    purple: string,
+    darkOrange: string,
+    red: string,
+    lightRed: string,
+    green: string,
+    blue: string
+}
